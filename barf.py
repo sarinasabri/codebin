3d1eaf73afd54c117d0bf9a637148e061ed45bef
Sina Mashek <mashek@thescoundrels.net>
2014-01-09 17:11:10 -0800

    Adding barf.

---
+++ barf.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Barf, a cli message system.
+#
+# Author: Sina Mashek <mashek@thescoundrels.net>
+# License: Expat, http://cptmashek.mit-license.org
+
+import datetime
+import time
+
+class Barf:
+	msg_codes = {
+		"DEF": '\033[0m    ',
+		"ACT": '\033[93m [~]',
+		"MSG": '\033[94m [-]',
+		"SAV": '\033[92m [#]',
+		"PLG": '\033[35m [*]',
+		"DBG": '\033[1;91m [$]',
+		"ERR": '\033[91m [!]',
+	}	
+
+	def __init__(self, code, message, time=True):
+		if code not in self.msg_codes:
+			code = "DEF"
+
+		print self.barf(code, message, time)
+
+	def disable(self):
+		DEF = ''
+		ACT = ''
+		MSG = ''
+		SAV = ''
+		PLG = ''
+		DBG = ''
+		ERR = ''
+
+	def get_time(self):
+		"""
+		Make time sexy
+		"""
+		return time.strftime("[%H:%M:%S] ", time.localtime(time.time()))
+
+	def get_time_for_file(self):
+		return "%s-%s" % (datetime.date.today(), time.strftime("%H%M%S",time.localtime(time.time())))
+
+	def barf(self, code, message, time):
+		if time == False:
+			return self.timeless_barf(code, message)
+		else:
+			return self.raw_barf(code, message)
+
+	def raw_barf(self, code, message):
+		return self.color(code, self.get_time() + message) + self.msg_codes["DEF"]
+
+	def timeless_barf(self, code, message):
+		return self.color(code, message) + self.msg_codes["DEF"]
+
+	def color(self, code, message):
+		return "%s %s" % (self.msg_codes[code], message)
+
---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env/python
+
+import barf
+
+code_keys = barf.Barf.msg_codes.keys()
+
+i=0
+for code in barf.Barf.msg_codes:
+	barf.Barf(code, "This be " + code_keys[i] + " message.")
+	barf.Barf(code, "This be " + code_keys[i] + " message without time.\n", False)
+	i += 1
+
+
+barf.Barf("ERR", "OMG AN ERROR")
